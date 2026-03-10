c53165ce9b090174bb08fd21b5b907474da04a2e
Erik Torck <eptorck@hotmail.com>
2026-03-10 09:37:06 +0100

    refactor inline form to its own component.

---
+++ quiz-client/app/form/FormComponent.tsx
@@ -0,0 +1,57 @@
+import Form from "next/form";
+import { questionViewModel } from "../viewmodels/questionViewModel";
+
+export interface FormComponentProps {
+  formSubmitHandler: (formData: FormData) => void;
+  questionViewModels: questionViewModel[];
+}
+export function FormComponent({
+  formSubmitHandler,
+  questionViewModels,
+}: FormComponentProps) {
+  return (
+    <Form action={formSubmitHandler}>
+      {questionViewModels.map((question) => (
+        <div key={question.id.toString()}>
+          <fieldset>
+            <legend className="questionTitle">
+              {decodeURIComponent(question.question)}
+            </legend>
+            {question.possibleAnswers.map((answer) => (
+              <div key={answer}>
+                <input
+                  type="radio"
+                  id={question.id.toString() + answer}
+                  name={question.id.toString()}
+                  value={answer}
+                  disabled={question.givenAnswer !== undefined}
+                  defaultChecked={question.givenAnswer === answer}
+                  required
+                />
+                <label htmlFor={question.id.toString() + answer}>
+                  {decodeURIComponent(answer)}
+                </label>
+              </div>
+            ))}
+          </fieldset>
+          {question.correctAnswer &&
+            question.givenAnswer === question.correctAnswer && (
+              <div className="correct">Correct!</div>
+            )}
+          {question.correctAnswer &&
+            question.givenAnswer !== question.correctAnswer && (
+              <div className="incorrect">
+                Incorrect! The correct answer is{" "}
+                {decodeURIComponent(question.correctAnswer)}
+              </div>
+            )}
+          <br />
+        </div>
+      ))}
+
+      <button type="submit" className="btn btn-success">
+        Submit
+      </button>
+    </Form>
+  );
+}
--- quiz-client/app/page.tsx
+++ quiz-client/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Form from "next/form";
 import { useState } from "react";
+import { FormComponent } from "./form/FormComponent";
 import { formSubmitHandler } from "./formSubmitHandler";
 import { questionViewModelMapper } from "./mappers/questionViewModelMapper";
 import { checkAnswerModel } from "./models/checkAnswerModel";
@@ -84,52 +84,11 @@ export default function Home() {
   });
 
   return (
-    <Form
-      action={(formData) =>
+    <FormComponent
+      formSubmitHandler={(formData) =>
         formSubmitHandler(formData, setCheckAnswerModels, setAnswers, setErrors)
       }
-    >
-      {questionViewModels.map((question) => (
-        <div key={question.id.toString()}>
-          <fieldset>
-            <legend className="questionTitle">
-              {decodeURIComponent(question.question)}
-            </legend>
-            {question.possibleAnswers.map((answer) => (
-              <div key={answer}>
-                <input
-                  type="radio"
-                  id={question.id.toString() + answer}
-                  name={question.id.toString()}
-                  value={answer}
-                  disabled={question.givenAnswer !== undefined}
-                  defaultChecked={question.givenAnswer === answer}
-                  required
-                />
-                <label htmlFor={question.id.toString() + answer}>
-                  {decodeURIComponent(answer)}
-                </label>
-              </div>
-            ))}
-          </fieldset>
-          {question.correctAnswer &&
-            question.givenAnswer === question.correctAnswer && (
-              <div className="correct">Correct!</div>
-            )}
-          {question.correctAnswer &&
-            question.givenAnswer !== question.correctAnswer && (
-              <div className="incorrect">
-                Incorrect! The correct answer is{" "}
-                {decodeURIComponent(question.correctAnswer)}
-              </div>
-            )}
-          <br />
-        </div>
-      ))}
-
-      <button type="submit" className="btn btn-success">
-        Submit
-      </button>
-    </Form>
+      questionViewModels={questionViewModels}
+    />
   );
 }
